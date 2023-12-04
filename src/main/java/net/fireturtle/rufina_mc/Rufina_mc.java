9e8f8b876961fa8570e88b0c25086cd558b0bc2c
fluo10 <fluo10.dev@fireturtle.net>
2023-12-05 08:48:03 +0900

    Add new item

--- src/main/java/net/fireturtle/rufina_mc/Rufina_mc.java
+++ src/main/java/net/fireturtle/rufina_mc/Rufina_mc.java
@@ -1,6 +1,11 @@
 package net.fireturtle.rufina_mc;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,13 +15,13 @@ public class Rufina_mc implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("rufina_mc");
-
+	public static final Item HALBERD = new Item(new FabricItemSettings());
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		Registry.register(Registries.ITEM, new Identifier("rufina_mc", "halberd"), HALBERD);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
