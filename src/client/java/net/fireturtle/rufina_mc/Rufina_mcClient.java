78d5dc3435489173577592dfddef515c4fba84de
fluo10 <fluo10.dev@fireturtle.net>
2023-12-05 07:36:00 +0900

    Add template

---
+++ src/client/java/net/fireturtle/rufina_mc/Rufina_mcClient.java
@@ -0,0 +1,10 @@
+package net.fireturtle.rufina_mc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Rufina_mcClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
