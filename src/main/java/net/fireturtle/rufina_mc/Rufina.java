190d8b5a241680cf1ee4fa7eaa42644989ced096
fluo10 <fluo10.dev@fireturtle.net>
2024-01-08 13:51:04 +0900

    Update Server started event.

--- src/main/java/net/fireturtle/rufina_mc/Rufina.java
+++ src/main/java/net/fireturtle/rufina_mc/Rufina.java
@@ -6,14 +6,31 @@ import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.predicate.entity.EntityPredicate;
+import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.TypeFilter;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.profiling.jfr.event.WorldLoadFinishedEvent;
+import net.minecraft.world.World;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class Rufina implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -21,12 +38,24 @@ public class Rufina implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("rufina_mc");
 	public static final Item HALBERD = new Item(new FabricItemSettings());
-
+	public static final String MOD_ID = "net.fireturtle.rufina_mc";
+	public static final Identifier RUFINA_UUID = new Identifier(MOD_ID, "rufina_uuid");
 	public static final EntityType<RufinaEntity> RUFINA = Registry.register(
 			Registries.ENTITY_TYPE,
-			new Identifier("fireturtle", "rufina"),
+			new Identifier(MOD_ID, "rufina"),
 			FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, RufinaEntity::new).dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build()
 	);
+
+	@Nullable
+	public RufinaEntity rufinaEntity = null;
+	
+	@Nullable
+	public PlayerEntity playerEntity = null;
+
+	@Nullable
+	public World rufinaWorld = null;
+
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -35,7 +64,36 @@ public class Rufina implements ModInitializer {
 		Registry.register(Registries.ITEM, new Identifier("rufina_mc", "halberd"), HALBERD);
 		LOGGER.info("Hello Fabric world!");
 		FabricDefaultAttributeRegistry.register(RUFINA, RufinaEntity.createMobAttributes());
+		ServerLifecycleEvents.SERVER_STARTED.register((server) -> {
+			RufinaPersistentState state = RufinaPersistentState.getServerState(server);
+			if (state.rufinaUuid == null) {
+				rufinaWorld = server.getOverworld();
+				RufinaEntity rufinaEntity = RUFINA.create(rufinaWorld);
+				BlockPos pos = rufinaWorld.getSpawnPos();
+				rufinaEntity.refreshPositionAndAngles(pos.getX(), pos.getY(), pos.getZ(), 0 , 0.0f);
 
+				rufinaWorld.spawnEntity(rufinaEntity);
+				LOGGER.info("Create Rufina Entity: {}", rufinaEntity.getUuidAsString());
+				state.rufinaUuid = rufinaEntity.getUuid();
+			} else {
+				Iterable<ServerWorld> worlds = server.getWorlds();
+
+				for (ServerWorld world : worlds) {
+					rufinaEntity = (RufinaEntity)world.getEntity(state.rufinaUuid);
+					if (rufinaEntity != null) {
+						LOGGER.info("Found Rufina Entity: {}", rufinaEntity.getUuidAsString());
+						rufinaWorld = world;
+						break;
+					}
+				}
+				if (rufinaEntity == null) {
+					LOGGER.info("Rufina entiti not found: {}", state.rufinaUuid);
+				}
+			}
+		});
+		
+		ServerTickEvents.END_SERVER_TICK.register((server) -> {
+		});
 	}
 
 }
--- src/main/java/net/fireturtle/rufina_mc/RufinaPersistentState.java
+++ src/main/java/net/fireturtle/rufina_mc/RufinaPersistentState.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.PersistentState;
+import net.minecraft.world.PersistentStateManager;
+import net.minecraft.world.World;
 
 public class RufinaPersistentState  extends PersistentState{
     @Nullable
@@ -19,12 +22,15 @@ public class RufinaPersistentState  extends PersistentState{
     static final String RUFINA_UUID_KEY = "rufinaUuid";
 
     static final String OWNER_UUID_KEY = "ownerUuid";
-    static final String RUFINA_WORLD_REGISTRY_KEY_KEY = "rufinaWorldKey";
     @Override
     public NbtCompound writeNbt(NbtCompound nbt) {
-        nbt.putUuid(RUFINA_UUID_KEY, rufinaUuid);
-        nbt.putUuid(OWNER_UUID_KEY, ownerUuid);
-        nbt.putString(RUFINA_WORLD_REGISTRY_KEY_KEY, rufinaWorldRegistryKey);
+        
+        if (rufinaUuid != null) {
+            nbt.putUuid(RUFINA_UUID_KEY, rufinaUuid);
+        }
+        if (ownerUuid != null) {
+            nbt.putUuid(OWNER_UUID_KEY, ownerUuid);
+        }
         return nbt;
     }
 
@@ -39,15 +45,35 @@ public class RufinaPersistentState  extends PersistentState{
         } else {
             this.ownerUuid = null;
         }
-        if(nbt.contains(RUFINA_WORLD_REGISTRY_KEY_KEY)) {
-            this.rufinaWorldRegistryKey = nbt.getString(RUFINA_WORLD_REGISTRY_KEY_KEY);
-            
-        }
     }
 
     public static RufinaPersistentState createFromNbt(NbtCompound tag) {
         RufinaPersistentState state = new RufinaPersistentState();
         state.readNbt(tag);
+        return state;
+    }
+    private static Type<RufinaPersistentState> type = new Type<>(
+            RufinaPersistentState::new, // If there's no 'StateSaverAndLoader' yet create one
+            RufinaPersistentState::createFromNbt, // If there is a 'StateSaverAndLoader' NBT, parse it with 'createFromNbt'
+            null // Supposed to be an 'DataFixTypes' enum, but we can just pass null
+    );
+ 
+    public static RufinaPersistentState getServerState(MinecraftServer server) {
+        // (Note: arbitrary choice to use 'World.OVERWORLD' instead of 'World.END' or 'World.NETHER'.  Any work)
+        PersistentStateManager persistentStateManager = server.getWorld(World.OVERWORLD).getPersistentStateManager();
+ 
+        // The first time the following 'getOrCreate' function is called, it creates a brand new 'StateSaverAndLoader' and
+        // stores it inside the 'PersistentStateManager'. The subsequent calls to 'getOrCreate' pass in the saved
+        // 'StateSaverAndLoader' NBT on disk to our function 'StateSaverAndLoader::createFromNbt'.
+        RufinaPersistentState state = persistentStateManager.getOrCreate(type, Rufina.MOD_ID);
+ 
+        // If state is not marked dirty, when Minecraft closes, 'writeNbt' won't be called and therefore nothing will be saved.
+        // Technically it's 'cleaner' if you only mark state as dirty when there was actually a change, but the vast majority
+        // of mod writers are just going to be confused when their data isn't being saved, and so it's best just to 'markDirty' for them.
+        // Besides, it's literally just setting a bool to true, and the only time there's a 'cost' is when the file is written to disk when
+        // there were no actual change to any of the mods state (INCREDIBLY RARE).
+        state.markDirty();
+ 
         return state;
     }
 }
